23abf9aab0476a1920705492bf50cb89ebed875b
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-11-10 07:40:39 +0530

    float changed to double

--- Assignment/Assignment 4/function_overloading.cpp
+++ Assignment/Assignment 4/function_overloading.cpp
@@ -9,8 +9,8 @@ class Calculator{
         int add(int a, int b, int c){
             return (a+b+c);
         }
-        double add(float a, float b, float c){
-            return (a+b+c);
+        double add(double a, double b){
+            return (a+b);
         }
 };
 

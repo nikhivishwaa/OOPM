c95c136d3c98cf8c837886495065dbe8df70f8e8
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-12-08 15:07:36 +0530

    All Experiments are performed

---
+++ Lab 10/Areaofrectangle.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class rectangle{
+    public:
+    void area_of_rectangle(int x, int y){
+        cout<<"\nArea : "<<x+y<<" square unit";
+    }
+    void area_of_rectangle(double x, double y){
+        cout<<"\nArea : "<<x*y<<" square unit";
+    }
+    void area_of_rectangle(int x, double y){
+        cout<<"\nArea : "<<x*y<<" square unit";
+    }
+    void area_of_rectangle(double x, int y){
+        cout<<"\nArea : "<<x*y<<" square unit";
+    }
+};
+
+int main(){
+    rectangle r1;
+    r1.area_of_rectangle(5,6);
+    r1.area_of_rectangle(4, 0.9);
+    r1.area_of_rectangle(3.2, 5);
+    r1.area_of_rectangle(4.1, 5.9);
+    return 0;
+}
---
+++ Lab 9/fun_ovrlod_.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class sample{
+    public:
+    void add(int x, int y){
+        cout<<"\nx + y : "<<x+y;
+    }
+    void add(double x, double y){
+        cout<<"\nx + y : "<<x+y;
+    }
+    void add(int x, int y, int z){
+        cout<<"\nx + y + z : "<<x+y+z;
+    }
+    void add(int x, int y, int z, int w){
+        cout<<"\nx + y + z + w : "<<x+y+z+w;
+    }  
+};
+
+int main(){
+    sample d1;
+    d1.add(5,6);
+    d1.add(5,6,9);
+    d1.add(5,4,6,9);
+    d1.add(5, 2, 6, 9);
+    return 0;
+}
